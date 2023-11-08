e21a543fefdc8bba16941dcf956ea9a53f2559ad
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-08 14:32:59 +0100

    start

---
+++ src/main/java/lezione22/Lezione22Application.java
@@ -0,0 +1,13 @@
+package lezione22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lezione22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lezione22Application.class, args);
+	}
+
+}
---
+++ src/test/java/lezione22/Lezione22ApplicationTests.java
@@ -0,0 +1,13 @@
+package lezione22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lezione22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
