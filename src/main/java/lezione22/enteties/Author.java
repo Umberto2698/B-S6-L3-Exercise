2e5ab20590582905ecba36b67eddcc1e21651e90
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-11-08 15:00:06 +0100

    Added Builder and Entity annotation

--- src/main/java/lezione22/enteties/Author.java
+++ src/main/java/lezione22/enteties/Author.java
@@ -1,19 +1,48 @@
 package lezione22.enteties;
 
+import com.github.javafaker.Faker;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Locale;
 import java.util.UUID;
+
 @Getter
 @ToString
 @Setter
+@Entity
+@Table(name = "authors")
+@Builder(builderClassName = "AuthorBuilder")
 public class Author {
+    @Id
     private UUID id;
     private String name;
     private String surname;
     private String email;
     private LocalDate birthday;
+    @Column(name = "author_img")
     private String avatar;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public static class AuthorBuilder {
+        private Faker faker = new Faker(Locale.ITALY);
+        private UUID id = UUID.randomUUID();
+        private String name = faker.name().firstName();
+        private String surname = faker.name().lastName();
+        private String email = name + "." + surname + "@faker.com";
+        private String avatar = "https://ui-avatars.com/api/?name=" + name + "+" + surname;
+        private LocalDate birthday = faker.date().birthday().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
 }
--- src/main/java/lezione22/enteties/BlogPost.java
+++ src/main/java/lezione22/enteties/BlogPost.java
@@ -1,19 +1,48 @@
 package lezione22.enteties;
 
+import com.github.javafaker.Faker;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Random;
 import java.util.UUID;
 
 @Getter
 @ToString
 @Setter
+@Entity
+@Table(name = "blog_posts")
+@Builder(builderClassName = "BlogPostBuiler")
 public class BlogPost {
+    @Id
     private UUID id;
     private String category;
+    @Column(name = "cover_img")
     private String cover;
     private String title;
     private String content;
+    @Column(name = "reading_time")
     private int readingTime;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public static class BlogPostBuiler {
+        private Faker faker = new Faker(Locale.ITALY);
+        private UUID id = UUID.randomUUID();
+        private String title = faker.book().title();
+        private String content = faker.lorem().characters(50);
+        private String category = faker.book().genre();
+        private String cover = "https://picsum.photos/200/300";
+        private int readingTime = new Random().nextInt(10, 240);
+    }
 }
